374e18bd8691e15e0085fddee4936ca0ce1b45bd
Fredrick Widjaya <f.widjaya20@gmail.com>
2019-07-18 20:47:10 +0700

    feature(model): adding base model

---
+++ example/model/genre.go
@@ -0,0 +1,17 @@
+package model
+
+import "github.com/fwidjaya20/goloquent/pkg/goloquent"
+
+// Genre .
+type Genre struct {
+	goloquent.Model `json:"-"`
+	ID              int64  `db:"id" json:"id"`
+	Name            string `db:"name" json:"name"`
+}
+
+// GenreModel .
+func GenreModel() *Genre {
+	return &Genre{
+		Model: goloquent.Model{},
+	}
+}
---
+++ pkg/goloquent/model.go
@@ -0,0 +1,9 @@
+package goloquent
+
+// IModel .
+type IModel interface {
+}
+
+// Model .
+type Model struct {
+}
